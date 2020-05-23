1d2bdd0ce192f2ea8e10ba1797f7b0d1223818b7
crackcell <tanmenglong@gmail.com>
2020-05-23 15:12:15 +0800

    add dwmstatus

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Sarasa Fixed SC Bold:size=11" };
-static const char dmenufont[]       = "Sarasa Fixed SC Bold:size=11";
+static const char *fonts[]          = { "Sarasa Fixed SC:size=11:weight=bold" };
+static const char dmenufont[]       = "Sarasa Fixed SC:size=11:weight=bold";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#828282";
